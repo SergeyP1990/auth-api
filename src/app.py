0bbbb686bb78cf45cb7d646c895722db7cf29deb
Sergey Plakunov <s_plakunov@eureca.ru>
2022-03-10 18:49:42 +0300

    Фунционирующая вьюха для register и начало login

--- src/app.py
+++ src/app.py
@@ -21,6 +21,7 @@ app = create_app()
 # Тестовая функция
 @app.route('/')
 def index():
+
     # admin = User(email='aa1@mail.ru', password='password', full_name='admin1', phone_number='79234567643')
     # db.session.add(admin)
     # db.session.commit()
--- src/service/user_logic.py
+++ src/service/user_logic.py
@@ -1,13 +1,22 @@
 from db.models import User
 from db.db import db
+from sqlalchemy import exc
 
 
 def register_new_user(user_login: str, password: str):
-    print("register_new_user called")
-    for login in db.session.query(User.email):
-        if login == user_login:
+    new_user = User(email=user_login, password=password)
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except exc.IntegrityError as Err:
+        # Constraint unique violation
+        if Err.orig.pgcode == "23505":
+            db.session.rollback()
             return "USER_EXISTS"
+        else:
+            raise
 
-    new_user = User(email=user_login, password=password, phone_number="123123")
-    db.session.add(new_user)
-    db.session.commit()
+
+def login_user(user_login: str, password: str):
+    for i in db.session.query(User).all():
+        pass
