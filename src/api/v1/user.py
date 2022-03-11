c6859d1b6a9387d300892c314e7dca04bc98efa6
SergeyP1990 <totalmeltdown90@gmail.com>
2022-03-11 14:20:37 +0300

    начало view для login и наброски view для role

--- src/api/v1/user.py
+++ src/api/v1/user.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, Response
-from service.user_logic import register_new_user
+from service.user_logic import register_new_user, login_user
 
 user_register = Blueprint("user_register", __name__, url_prefix="/user")
 
@@ -18,5 +18,23 @@ def register():
         result = register_new_user(username, password)
         if result == "USER_EXISTS":
             return Response(status=409, mimetype="application/json")
+
+        return Response(status=200, mimetype="application/json")
+
+
+@user_register.route("/login", methods=["POST"])
+def login():
+    if request.method == "POST":
+        request_data = request.get_json()
+
+        username = request_data["username"]
+        password = request_data["password"]
+
+        if username is None or password is None:
+            return Response(status=400, mimetype="application/json")
+
+        result = login_user(username, password)
+        if result == "AUTH_FAILED":
+            return Response(status=401, mimetype="application/json")
 
         return Response(status=200, mimetype="application/json")
--- src/service/user_logic.py
+++ src/service/user_logic.py
@@ -1,22 +1,32 @@
 from db.models import User
 from db.db import db
 from sqlalchemy import exc
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def register_new_user(user_login: str, password: str):
-    new_user = User(email=user_login, password=password)
-    try:
-        db.session.add(new_user)
-        db.session.commit()
-    except exc.IntegrityError as Err:
-        # Constraint unique violation
-        if Err.orig.pgcode == "23505":
-            db.session.rollback()
-            return "USER_EXISTS"
-        else:
-            raise
+
+    user = User.query.filter_by(email=user_login).first()
+    if user:
+        return "USER_EXISTS"
+    hashed_pass = generate_password_hash(password)
+    new_user = User(email=user_login, password=hashed_pass)
+
+    # try:
+    db.session.add(new_user)
+    db.session.commit()
+    # except exc.IntegrityError as Err:
+    #     # Constraint unique violation
+    #     if Err.orig.pgcode == "23505":
+    #         db.session.rollback()
+    #
+    #     else:
+    #         raise
 
 
 def login_user(user_login: str, password: str):
-    for i in db.session.query(User).all():
-        pass
+
+    user = User.query.filter_by(email=user_login).first()
+
+    if not user or not check_password_hash(user.password, password):
+        return "AUTH_FAILED"
