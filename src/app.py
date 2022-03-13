f6da9d1787535a5187ccc0d40ad8c26286ea7223
SergeyP1990 <totalmeltdown90@gmail.com>
2022-03-13 18:40:05 +0300

    работа с jwt и рабочий view для login

--- src/app.py
+++ src/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from db.db import init_db, db
 from db.models import User
+from flask_jwt_extended import JWTManager
+
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
+from service.user_logic import jwt
 
 
 def create_app():
@@ -9,21 +15,26 @@ def create_app():
     app.app_context().push()
     db.create_all()
 
+    # Here you can globally configure all the ways you want to allow JWTs to
+    # be sent to your web application. By default, this will be only headers.
+    app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+
+    # If true this will only allow the cookies that contain your JWTs to be sent
+    # over https. In production, this should always be set to True
+    app.config["JWT_COOKIE_SECURE"] = False
+
+    # Change this in your code!
+    app.config["JWT_SECRET_KEY"] = "super-secret"
+
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(seconds=5)
+    app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(seconds=7)
+
     from api.v1 import user
-    app.register_blueprint(user.user_register)
+    app.register_blueprint(user.user)
+
+    jwt.init_app(app)
 
     return app
 
 
 app = create_app()
-
-
-# Тестовая функция
-@app.route('/')
-def index():
-
-    # admin = User(email='aa1@mail.ru', password='password', full_name='admin1', phone_number='79234567643')
-    # db.session.add(admin)
-    # db.session.commit()
-    return 'index page'
-
