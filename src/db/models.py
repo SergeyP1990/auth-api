6e65ade04e8dc54e8e0d16d131cf32f6eb9c9fa5
Artem Osokin <aptem21@mail.ru>
2022-03-15 20:15:25 +0300

    Поправлены связи моделей

--- src/db/models.py
+++ src/db/models.py
@@ -30,8 +30,16 @@ class User(MixinIdDate):
     full_name = db.Column(db.String(length=256))
     phone_number = db.Column(db.String(length=12))
     auth_history = db.relationship('AuthHistory',
-                                   backref='users',
-                                   lazy='dynamic')
+                                   backref='user',
+                                   lazy='dynamic',
+                                   cascade='all, delete-orphan',
+                                   passive_deletes=True
+                                   )
+    role_user = db.relationship('UserRole',
+                                backref='user',
+                                lazy='dynamic',
+                                cascade='all, delete-orphan',
+                                passive_deletes=True)
 
     def __repr__(self):
         return f'<User {self.login}>'
@@ -41,6 +49,11 @@ class Role(MixinIdDate):
     __tablename__ = 'roles'
 
     name = db.Column(db.String, unique=True, nullable=False)
+    role_user = db.relationship('UserRole',
+                                backref='role',
+                                lazy='dynamic',
+                                cascade='all, delete-orphan',
+                                passive_deletes=True)
 
     def __repr__(self):
         return f'<Role {self.name}>'
@@ -49,13 +62,22 @@ class Role(MixinIdDate):
 class UserRole(MixinIdDate):
     __tablename__ = 'user_role'
     __table_args__ = (UniqueConstraint("user_id", "role_id"),)
-    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey("users.id"))
-    role_id = db.Column(UUID(as_uuid=True), db.ForeignKey("roles.id"))
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey(
+        "users.id",
+        ondelete='CASCADE'
+    ))
+    role_id = db.Column(UUID(as_uuid=True), db.ForeignKey(
+        "roles.id",
+        ondelete='CASCADE'
+    ))
 
 
 class AuthHistory(MixinIdDate):
     __tablename__ = 'auth_history'
-    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey("users.id"))
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey(
+        "users.id",
+        ondelete='CASCADE'
+    ))
     user_agent = db.Column(db.String, nullable=False)
     host = db.Column(db.String, nullable=False)
     auth_result = db.Column(db.String, nullable=False)
