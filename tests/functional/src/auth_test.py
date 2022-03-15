dadc3812d0248675399e3e6e60b3560e60da7674
Artem Osokin <aptem21@mail.ru>
2022-03-15 23:29:24 +0300

    Написание тестов для user

--- tests/functional/src/auth_test.py
+++ tests/functional/src/auth_test.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import psycopg2
 from http import HTTPStatus
 
 import requests
@@ -47,8 +48,65 @@ def test_login():
     assert res.status_code == HTTPStatus.OK
 
 
+def test_update_email_or_password():
+    with psycopg2.connect(**test_settings.dsl) as pg_conn:
+        curr = pg_conn.cursor()
+        curr.execute("""SELECT id FROM users WHERE email='user1@mail.com';""")
+        id = curr.fetchone()
+        pg_conn.commit()
+        curr.close()
+    body = json.dumps({
+        "id": id[0],
+        "username": "user2@mail.com",
+        "password": "P@ssw0rd2"
+    })
+    headers = {'Content-Type': 'application/json'}
+    res = requests.put(
+        url=f'{test_settings.auth_api_host}/user',
+        data=body,
+        headers=headers
+    )
+    assert res.status_code == HTTPStatus.OK
+    body = json.dumps({
+        "id": id[0],
+        "username": "user2@mail.com",
+        "password": "P@ssw0rd2"
+    })
+    res = requests.put(
+        url=f'{test_settings.auth_api_host}/user/',
+        data=body,
+        headers=headers
+    )
+    assert res.status_code == HTTPStatus.CONFLICT
+    body = json.dumps({
+        "id": id[0],
+        "username": "user1@mail.com",
+        "password": "P@ssw0rd2"
+    })
+    res = requests.put(
+        url=f'{test_settings.auth_api_host}/user/',
+        data=body,
+        headers=headers
+    )
+    assert res.status_code == HTTPStatus.OK
+
+
+def test_refresh_access():
+    res = requests.post(
+        url=f'{test_settings.auth_api_host}/user/refresh',
+    )
+    assert res.status_code == HTTPStatus.OK
+
+
 def test_auth_history():
     res = requests.get(
         url=f'{test_settings.auth_api_host}/user/auth_history',
+    )
+    assert res.status_code == HTTPStatus.OK
+
+
+def test_logout():
+    res = requests.post(
+        url=f'{test_settings.auth_api_host}/user/logout'
     )
     assert res.status_code == HTTPStatus.OK
