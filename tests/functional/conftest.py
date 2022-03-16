e65660e60e3858f50ac45478b67791ab18297df6
Artem Osokin <aptem21@mail.ru>
2022-03-16 15:00:52 +0300

    Написаны тестов для auth_api

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -12,5 +12,22 @@ def truncate_table():
     with psycopg2.connect(**test_settings.dsl) as pg_conn:
         curr = pg_conn.cursor()
         curr.execute("""DELETE FROM users WHERE email='user1@mail.com';""")
+        curr.execute("""DELETE FROM users WHERE email='user2@mail.com';""")
+        curr.execute("""DELETE FROM roles WHERE name='superadmin';""")
+        pg_conn.commit()
+        curr.close()
+
+
+@pytest.fixture()
+def grunt_user_role():
+    with psycopg2.connect(**test_settings.dsl) as pg_conn:
+        curr = pg_conn.cursor()
+        curr.execute("""CREATE EXTENSION IF NOT EXISTS "uuid-ossp";""")
+        curr.execute("""INSERT INTO roles VALUES (uuid_generate_v4(), now(),now(), 'superadmin');""")
+        curr.execute("""SELECT id FROM roles WHERE name='superadmin';""")
+        role_id = curr.fetchone()
+        curr.execute("""SELECT id FROM users WHERE email='user1@mail.com';""")
+        user_id = curr.fetchone()
+        curr.execute(f"""INSERT INTO user_role VALUES (uuid_generate_v4(), now(),now(),'{user_id[0]}','{role_id[0]}');""")
         pg_conn.commit()
         curr.close()
--- tests/functional/src/auth_test.py
+++ tests/functional/src/auth_test.py
@@ -48,65 +48,179 @@ def test_login():
     assert res.status_code == HTTPStatus.OK
 
 
-def test_update_email_or_password():
+def test_refresh_access():
+    body = json.dumps({"email": "user1@mail.com", "password": "P@ssw0rd"})
+    headers = {'Content-Type': 'application/json'}
+    res = requests.post(
+        url=f'{test_settings.auth_api_host}/user/login',
+        data=body,
+        headers=headers
+    )
+    csrf_access = res.cookies['csrf_access_token']
+    csrf_refresh = res.cookies['csrf_refresh_token']
+    cookies = res.cookies
+    headers = {'Content-Type': 'application/json',
+               'X-CSRF-TOKEN': csrf_access,
+               'X-CSRF-TOKEN-REF': csrf_refresh}
+    res = requests.post(
+        url=f'{test_settings.auth_api_host}/user/refresh',
+        cookies=cookies,
+        headers=headers
+    )
+    assert res.status_code == HTTPStatus.OK
+
+
+def test_auth_history():
+    body = json.dumps({"email": "user1@mail.com", "password": "P@ssw0rd"})
+    headers = {'Content-Type': 'application/json'}
+    res = requests.post(
+        url=f'{test_settings.auth_api_host}/user/login',
+        data=body,
+        headers=headers
+    )
+
+    csrf_access = res.cookies['csrf_access_token']
+    csrf_refresh = res.cookies['csrf_refresh_token']
+    cookies = res.cookies
+    headers = {'Content-Type': 'application/json',
+               'X-CSRF-TOKEN': csrf_access,
+               'X-CSRF-TOKEN-REF': csrf_refresh}
+    res = requests.get(
+        url=f'{test_settings.auth_api_host}/user/auth_history',
+        cookies=cookies,
+        headers=headers
+    )
+    assert len(res.content) > 0
+    assert res.status_code == HTTPStatus.OK
+
+
+def test_logout():
+    body = json.dumps({"email": "user1@mail.com", "password": "P@ssw0rd"})
+    headers = {'Content-Type': 'application/json'}
+    res = requests.post(
+        url=f'{test_settings.auth_api_host}/user/login',
+        data=body,
+        headers=headers
+    )
+
+    csrf_access = res.cookies['csrf_access_token']
+    csrf_refresh = res.cookies['csrf_refresh_token']
+    cookies = res.cookies
+    headers = {'Content-Type': 'application/json',
+               'X-CSRF-TOKEN': csrf_access,
+               'X-CSRF-TOKEN-REF': csrf_refresh}
+
+    res = requests.post(
+        url=f'{test_settings.auth_api_host}/user/logout',
+        cookies=cookies,
+        headers=headers
+    )
+    assert res.status_code == HTTPStatus.OK
+
+
+def test_update_email_or_password_ok(grunt_user_role):
+    body = json.dumps({"email": "user1@mail.com", "password": "P@ssw0rd"})
+    headers = {'Content-Type': 'application/json'}
+    res = requests.post(
+        url=f'{test_settings.auth_api_host}/user/login',
+        data=body,
+        headers=headers
+    )
+    assert res.status_code == HTTPStatus.OK
+
     with psycopg2.connect(**test_settings.dsl) as pg_conn:
         curr = pg_conn.cursor()
         curr.execute("""SELECT id FROM users WHERE email='user1@mail.com';""")
         id = curr.fetchone()
         pg_conn.commit()
         curr.close()
+
     body = json.dumps({
-        "id": id[0],
-        "username": "user2@mail.com",
+        "user_id": id[0],
+        "email": "user2@mail.com",
         "password": "P@ssw0rd2"
     })
-    headers = {'Content-Type': 'application/json'}
+
+    csrf_access = res.cookies['csrf_access_token']
+    csrf_refresh = res.cookies['csrf_refresh_token']
+    cookies = res.cookies
+    headers = {'Content-Type': 'application/json',
+               'X-CSRF-TOKEN': csrf_access,
+               'X-CSRF-TOKEN-REF': csrf_refresh}
     res = requests.put(
         url=f'{test_settings.auth_api_host}/user',
         data=body,
-        headers=headers
+        headers=headers,
+        cookies=cookies
     )
     assert res.status_code == HTTPStatus.OK
-    body = json.dumps({
-        "id": id[0],
-        "username": "user2@mail.com",
-        "password": "P@ssw0rd2"
-    })
-    res = requests.put(
-        url=f'{test_settings.auth_api_host}/user/',
-        data=body,
-        headers=headers
-    )
-    assert res.status_code == HTTPStatus.CONFLICT
-    body = json.dumps({
-        "id": id[0],
-        "username": "user1@mail.com",
-        "password": "P@ssw0rd2"
-    })
-    res = requests.put(
-        url=f'{test_settings.auth_api_host}/user/',
+
+
+def test_update_email_or_password_not_found():
+    body = json.dumps({"email": "user2@mail.com", "password": "P@ssw0rd2"})
+    headers = {'Content-Type': 'application/json'}
+    res = requests.post(
+        url=f'{test_settings.auth_api_host}/user/login',
         data=body,
         headers=headers
     )
     assert res.status_code == HTTPStatus.OK
 
+    csrf_access = res.cookies['csrf_access_token']
+    csrf_refresh = res.cookies['csrf_refresh_token']
+    cookies = res.cookies
 
-def test_refresh_access():
+    headers = {'Content-Type': 'application/json',
+               'X-CSRF-TOKEN': csrf_access,
+               'X-CSRF-TOKEN-REF': csrf_refresh}
+    body = json.dumps({
+        "user_id": "2e2c309a-1111-2222-9364-6090056ad998",
+        "email": "11111@mail.com",
+        "password": "P@ssw0rd2"
+    })
+
+    res = requests.put(
+        url=f'{test_settings.auth_api_host}/user/',
+        data=body,
+        headers=headers,
+        cookies=cookies
+    )
+    assert res.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_update_email_or_password_conflict():
+    body = json.dumps({"email": "user2@mail.com", "password": "P@ssw0rd2"})
+    headers = {'Content-Type': 'application/json'}
     res = requests.post(
-        url=f'{test_settings.auth_api_host}/user/refresh',
+        url=f'{test_settings.auth_api_host}/user/login',
+        data=body,
+        headers=headers
     )
     assert res.status_code == HTTPStatus.OK
 
+    with psycopg2.connect(**test_settings.dsl) as pg_conn:
+        curr = pg_conn.cursor()
+        curr.execute("""DELETE FROM roles WHERE name='superadmin';""")
+        curr.execute("""SELECT id FROM users WHERE email='user2@mail.com';""")
+        id = curr.fetchone()
+        pg_conn.commit()
+        curr.close()
 
-def test_auth_history():
-    res = requests.get(
-        url=f'{test_settings.auth_api_host}/user/auth_history',
+    csrf_access = res.cookies['csrf_access_token']
+    csrf_refresh = res.cookies['csrf_refresh_token']
+    cookies = res.cookies
+    headers = {'Content-Type': 'application/json',
+               'X-CSRF-TOKEN': csrf_access,
+               'X-CSRF-TOKEN-REF': csrf_refresh}
+    body = json.dumps({
+        "user_id": id[0],
+        "email": "user2@mail.com",
+        "password": "P@ssw0rd2"
+    })
+    res = requests.put(
+        url=f'{test_settings.auth_api_host}/user/',
+        data=body,
+        headers=headers,
+        cookies=cookies
     )
-    assert res.status_code == HTTPStatus.OK
-
-
-def test_logout():
-    res = requests.post(
-        url=f'{test_settings.auth_api_host}/user/logout'
-    )
-    assert res.status_code == HTTPStatus.OK
+    assert res.status_code == HTTPStatus.FORBIDDEN
